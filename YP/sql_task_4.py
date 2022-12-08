e0146f92306d3e3a35ffd3abef894590fdecc0e1
Байбак Кирилл lin <zloifox@gmail.com>
2022-12-08 20:45:44 +0300

    one to one

---
+++ YP/sql_task_4.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+con = sqlite3.connect('db.sqlite')
+cur = con.cursor()
+
+"""
+CREATE TABLE IF NOT EXISTS wrappers(
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ice_cream(
+id INTEGER PRIMARY KEY,
+name TEXT,
+description TEXT,
+wrapper_id INTEGER UNIQUE,
+FOREIGN KEY(wrapper_id) REFERENCES wrappers(id)
+);
+
+INSERT INTO wrappers
+VALUES
+(1, 'Бумажная с черепами');
+
+INSERT INTO ice_cream(id, name, description)
+VALUES
+(1, 'Пивное мороженое', 'Со вкусом светлого нефильтрованного лагера.'),
+(2, 'Мороженое с кузнечиками', 'В колумбийском стиле: с добавлением карамелизованных кузнечиков.'),
+(3, 'Мороженое со вкусом сыра чеддер', 'Вкус настоящего сыра в вафельном стаканчике.'),
+(4, 'Пломбир 1937', 'Пломбир по рецепту 1937 года Московского хладокомбината.');
+INSERT INTO ice_cream
+VALUES
+(5, 'Томатное мороженое', 'В СССР делали и томатное мороженое тоже. Вкус специфический.', 1);
+"""
+
+# ice.name and wrapper.name
+cur.execute('''
+SELECT ice_cream.name,
+       wrappers.name
+FROM ice_cream,
+     wrappers
+WHERE ice_cream.wrapper_id = wrappers.id AND
+      wrappers.name = 'Бумажная с черепами';
+''')
+
+for result in cur:
+    print(result)
+
+con.commit()
+con.close()
