f8447abef10834ffae25c205c71538d49b80d7b2
Байбак Кирилл lin <zloifox@gmail.com>
2023-03-25 14:34:05 +0300

    sql: count the number of cubes

---
+++ Codewars_SQL/8kyu/count_number_of_cubes.sql
@@ -0,0 +1,27 @@
+-- Your function takes as parameter the number of times the cube has been cut.
+-- You must return the number of smaller cubes created by
+-- the cuts that have at least one red face.
+
+-- Examples:
+-- countSquares(2) --> 26
+-- countSquares(4) --> 98
+
+-- PostgresSQL
+
+CREATE TABLE IF NOT EXISTS squares(
+    id serial PRIMARY KEY,
+	n INT
+);
+INSERT INTO squares(n)
+VALUES
+    (0),
+    (5),
+    (16),
+    (23);
+
+SELECT n,
+CASE
+    WHEN n = 0 THEN 1
+    ELSE POWER(n + 1, 3) - POWER((n - 1), 3)
+END::int AS res
+FROM squares
